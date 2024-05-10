d2c887abc00a56e428fd7848044bab27d1709cbe
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2024-05-10 13:12:06 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,59 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        key_hash = self.hash_function(key)
+        key_value = [key, value]
+
+        if self.table[key_hash] is None:
+            self.table[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.table[key_hash].append(key_value)
+            return True
+
+    def get(self, key):
+        key_hash = self.hash_function(key)
+        print('get key_hash', key_hash)
+        if self.table[key_hash] is not None:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    def delete(self, key):
+        key_hash = self.hash_function(key)
+
+        if self.table[key_hash] is not None:
+            for pair in self.table[key_hash]:
+                if pair[0] == key:
+                    self.table[key_hash].remove(pair)
+                    return pair[1]
+
+        print('There is no such a key in table')
+
+
+# Тестуємо нашу хеш-таблицю:
+H = HashTable(5)
+H.insert("apple", 10)
+H.insert("orange", 20)
+H.insert("banana", 30)
+
+print('\n', H.table)
+#
+print(H.get("apple"))   # Виведе: 10
+print(H.get("orange"))  # Виведе: 20
+print(H.get("banana"))  # Виведе: 30
+
+H.delete("applfe") # Виведе: There is no such a key in table
+H.delete("apple")
+print('\n', H.table) # Виведе список без пари, яка включає 'apple'
